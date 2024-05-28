5f46ed8d3ec69ea5d4da3ca3ddccad832e127750
flaviosenne <flaviosenne123@gmail.com>
2024-05-28 08:52:49 -0300

    requisições assincronas com axios

--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -3,6 +3,13 @@
         <h1 class="text-lg font-bold">Produtos</h1>
         <NuxtLink to="/produtos/produto-a">Produto A</NuxtLink>
         <NuxtLink to="/produtos/produto-b">Produto B</NuxtLink>
+
+        <br /><br />
+
+        <div v-for="product in products" :key="product.id" class="border-b border-gray-400 py-3">
+            {{ product.title }}
+        </div>
+
     </div>
 </template>
 
@@ -12,6 +19,13 @@ export default {
     layout: '',
     data() {
         return {}
+    },
+    async asyncData({ $axios }) {
+
+        const products = await $axios.$get('https://jsonplaceholder.typicode.com/posts?_limit=3')
+        return {
+            products
+        }
     }
 }
 </script>
